a4c7d646783fe4bea85374ab3a8490ad96ef4491
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-05-18 08:20:10 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit  # exit on error
 
 pip install -r requirements.txt
-cd /DocumentSearch/
+cd DocumentSearch/
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py populate_db
