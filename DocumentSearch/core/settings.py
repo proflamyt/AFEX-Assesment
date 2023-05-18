e2b3c79ef96e044bcfec65d2cef776f30411a26d
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-05-18 08:58:57 +0100

    rename remote

--- DocumentSearch/core/settings.py
+++ DocumentSearch/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = getenv('HOSTS')
+ALLOWED_HOSTS = [getenv('HOSTS')]
 
 
 # Application definition
